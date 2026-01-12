76a59478f4a8925d20157f8ee23816a83473424e
Ke Wang <wangke20021@outlook.com>
2026-01-12 10:56:55 -0800

    Add init

--- envs/__init__.py
+++ envs/__init__.py
@@ -3,5 +3,7 @@ Environments Package
 
 Provides drone simulation environments for navigation.
 """
-
 from .drone_env import SimpleDroneEnv
+from .expert_env import ExpertDroneEnv
+
+__all__ = ['SimpleDroneEnv', 'ExpertDroneEnv']
